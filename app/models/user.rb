118954836d4b3543e8f9617aa7380a88434c917f
Brianna <briannakincart@gmail.com>
2017-08-19 19:22:45 -0400

    Set up Facebook OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
+  def self.create_from_omniauth(auth)
+    binding.pry
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # Need to update the below once I can look at the actual auth hash and what's available
+      user.first_name = auth.info.name
+      user.last_name = auth.info.name
+    end
+  end
 end
