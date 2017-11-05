aa3a6b087572fc219ef1f35b9f7c2c0630cb6447
Brianna <briannakincart@gmail.com>
2017-11-05 14:46:39 -0500

    Add React Router with various pages

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Home from './components/Home';
+import OtherPage from './components/OtherPage';
+import DynamicPage from './components/DynamicPage';
 
 const App = props => {
   return(
-    <h1>This is React!</h1>
+    <Router history={browserHistory}>
+      <Route path='/' component={Home} />
+      <Route path='/other' component={OtherPage} />
+      <Route path='/:dynamic' component={DynamicPage} />
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ app/javascript/react/src/components/DynamicPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DynamicPage = props => {
+  return(
+    <h1>This is the dynamic page!</h1>
+  )
+}
+
+export default DynamicPage;
