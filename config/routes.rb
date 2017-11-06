f0ff28b6ae80c646f5515652a67b7d61165171d7
Brianna <briannakincart@gmail.com>
2017-11-05 19:29:18 -0500

    Set up current_user API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks'}
 
+  namespace :api do
+    namespace :v1 do
+      resources :current_user, only: :index
+    end
+  end
+
   root 'home#index'
   get '*path', to: 'home#index'
 end
